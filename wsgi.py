e5b6ffb63cd159978577497b24381690c07f09a5
YankoFelipe <yankofelipe.af@gmail.com>
2020-03-23 23:09:39 -0300

    Adding different average methods

--- wsgi.py
+++ wsgi.py
@@ -3,15 +3,19 @@ import sched
 import time
 
 from binance.client import Client
+from average import Average
 
 client = Client(getenv("API_KEY"), getenv("SECRET_KEY"))
 s = sched.scheduler(time.time, time.sleep)
 symbol = getenv("SYMBOL")
-delay = 2  # Seconds
+delay = 5  # Seconds
 
 
 def print_price(sc):
-    print(client.get_avg_price(symbol=symbol)['price'])
+    try:
+        print(Average.get_average_first(client, 'BTCUSDT'))
+    except:
+        print('Client error')
     s.enter(delay, 1, print_price, ('first',))
 
 
